d321325303132edca97cde91ad18bdcb64e6107f
Артем <artem2706@ngs.ru>
2021-09-14 11:09:28 +0700

    edit template, add docstring in utils, add upload file extensions

--- app_image_analyzer/forms.py
+++ app_image_analyzer/forms.py
@@ -5,10 +5,18 @@ from django.core.validators import RegexValidator, FileExtensionValidator
 class UploadImageForm(forms.Form):
     image = forms.ImageField(
         label='Файл изображения',
-        validators=[FileExtensionValidator(allowed_extensions=('jpg', 'png'))])
+        validators=[
+            FileExtensionValidator(allowed_extensions=(
+                'jpg', 'jpeg', 'gif', 'bmp', 'png',
+            ))])
     hex_code = forms.CharField(
-        max_length=7, label='HEX-код искомого цвета', required=False,
-        validators=[RegexValidator(regex=
-                                   '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')],
-        error_messages={'invalid': 'Не корректный HEX-код. '
-                                   'Примеры: #000000, #FFFFFF, #fff, #000'})
+        max_length=7,
+        label='HEX-код искомого цвета',
+        required=False,
+        validators=[
+            RegexValidator(regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
+        ],
+        error_messages={
+            'invalid': 'Не корректный HEX-код. Примеры: #FFFFFF, #fff, #000'
+        },
+    )
--- app_image_analyzer/utils.py
+++ app_image_analyzer/utils.py
@@ -1,21 +1,33 @@
-from io import BytesIO
 from typing import Tuple, Optional
 
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from PIL import Image
 
 BLACK_PIXEL = (0, 0, 0)
 WHITE_PIXEL = (255, 255, 255)
 
 
-def get_analysis_result(image_file: BytesIO, hex_code: str = None) -> \
+def get_analysis_result(image_file: InMemoryUploadedFile,
+                        hex_code: str = None) -> \
         Tuple[int, int, Optional[int]]:
+    """
+    Производит анализ цветов в изображении, считает количество пискелей белого,
+    черного и искомого цветов
+
+    :param image_file: загруженный файл изображения
+    :param hex_code: HEX-код искомого цвета, если не задан,
+    то поиск не производится
+    :return: количество черных, белых пискселей и количество пикселей искогомо
+    цвета, если цвет не был задан то None
+    """
     search_color = hex_to_tuple(hex_code) if hex_code else None
     with Image.open(image_file).convert('RGB') as image:
+
         black_pixel_count: int = 0
         white_pixel_count: int = 0
         search_color_count: int = 0 if search_color else None
         for pixel in image.getdata():
-            if pixel == search_color:
+            if search_color and pixel == search_color:
                 search_color_count += 1
             if pixel == BLACK_PIXEL:
                 black_pixel_count += 1
@@ -25,6 +37,12 @@ def get_analysis_result(image_file: BytesIO, hex_code: str = None) -> \
 
 
 def hex_to_tuple(hex_code: str) -> Tuple[int, int, int]:
+    """
+    Переводит HEX-код в decimal-код цвета в виде кортежа (R, G, B)
+
+    :param hex_code: HEX-код цвета в виде строки (#FFFFFF, #FFF)
+    :return: decimal-код цвета в виде кортежа (R, G, B)
+    """
     hex_code = hex_code[1:]
     if len(hex_code) == 3:
         hex_code = ''.join([2 * ch for ch in hex_code])
